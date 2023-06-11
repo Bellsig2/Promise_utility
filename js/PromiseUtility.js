72997cd8d4c62b429652b5623dc989b7ff3c5aee
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2023-06-11 17:28:11 +0900

    promise 유틸리티 함수
    all, any, sequantial 메서드 구현

--- js/PromiseUtility.js
+++ js/PromiseUtility.js
@@ -1,16 +1,40 @@
-function PromiseUtility() {
-  this.promiseAll = function (promises) {
-    let results = [];
-    let promsie = new Promise((resolve, reject) => {
-      promises.forEach((promise) => {
-        promise.then((value) => {
-          results.push(value);
-        });
-
-        if (results.length === promises.length) {
-          resolve(results);
+export default function PromiseUtility() {
+  let results = [];
+  let rejects = [];
+  this.all = function (promises) {
+    return new Promise((resolve, reject) => {
+      promises.forEach(
+        (promise) => {
+          promise.then((value) => {
+            results.push(value);
+            if (results.length === promises.length) {
+              resolve(results);
+            }
+          });
+        },
+        (error) => reject(error)
+      );
+    });
+  };
+  this.any = function (promises) {
+    return new Promise((resolve, reject) => {
+      promises.forEach(
+        (promise) => {
+          promise.then((value) => {
+            resolve(value);
+          });
+        },
+        (error) => {
+          rejects.push(error);
+          rejects.length === promises.length ? reject(error) : "";
         }
-      }, reject);
+      );
+    });
+  };
+  this.sequantial = function (promises) {
+    promises.forEach(async (promise) => {
+      let result = await promise;
+      console.log(result);
     });
   };
 }
